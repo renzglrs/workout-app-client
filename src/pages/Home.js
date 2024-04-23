07a86051fc2b03129cf6e857f8908a227957dff9
renzcarlo.glorioso <renzcarlo.glorioso@gmail.com>
2024-04-23 10:52:47 +0800

    Added .env.local

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('/api/workouts');
+      const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/workouts`);
       const json = await response.json();
 
       if (response.ok) {
